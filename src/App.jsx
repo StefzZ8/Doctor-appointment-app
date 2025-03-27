e019675acf7e8e586450da3bf55d76694c6790f3
Stefko1998 <Simjanovski998@gmail.com>
2025-03-27 21:28:44 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+const App = () => {
+
+  return (
+    <>
+      <h1>Doctor Appointment</h1>
+    </>
+  )
+}
+
+export default App
