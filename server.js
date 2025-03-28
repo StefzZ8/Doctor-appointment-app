5287541b7d8eabd56995319baf7a68fe1aaf1dcd
Stefko1998 <Simjanovski998@gmail.com>
2025-03-28 13:44:11 +0100

    imported cors middleware for connection between front and back and adding routes

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-import express from 'express';
 import 'dotenv/config';
 import './config/dbConfig.js';
+import cors from 'cors'; // Import CORS middleware
+import userRoute from './routes/userRoute.js';
+import express from 'express';
 const app = express();
 
+app.use(cors());
+
+app.use(express.json());
+app.use('/api/user', userRoute);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
