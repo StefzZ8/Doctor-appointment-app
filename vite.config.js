8ba7c5f5d1ed9d90908df30f53f996399d6b0210
Stefko1998 <Simjanovski998@gmail.com>
2025-03-28 13:47:55 +0100

    proxy connecting

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { port: 3000 },
+  proxy: {
+    '/api': 'http://localhost:5000', // Proxy API calls to the backend
+  },
 })
