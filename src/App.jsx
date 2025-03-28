0ccf61c296ad289e0a01d3a9627c342005c770c5
Stefko1998 <Simjanovski998@gmail.com>
2025-03-28 14:00:17 +0100

    routes to pages-navigation, toaster for msgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './components/Login'
+import Register from './components/Register'
+import Home from './components/Home'
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
 
   return (
-    <>
-      <h1>Doctor Appointment</h1>
-    </>
+
+    <Router>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+      />
+      <Routes>
+
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/' element={<Home />} />
+
+      </Routes>
+    </Router>
   )
 }
 
-export default App
+export default App;
