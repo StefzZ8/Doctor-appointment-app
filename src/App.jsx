f4f3ff30f474445ac8d51ea442b5c66eb9cb61e6
Stefko1998 <Simjanovski998@gmail.com>
2025-03-29 19:37:44 +0100

    Implemented auth routing with ProtectedRoute & PublicRoute, added loading spinner and toast notifications.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,47 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './components/Login'
-import Register from './components/Register'
-import Home from './components/Home'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
 import { Toaster } from 'react-hot-toast';
+import { useSelector } from 'react-redux';
+import ProtectedRoute from './components/ProtectedRoute';
+import PublicRoute from './components/PublicRoute';
 
 const App = () => {
+  const { loading } = useSelector((state) => state.alerts);
 
   return (
 
     <Router>
+      {loading && (
+        <div className='spinner-parent'>
+          <div class="spinner-border" role="status">
+          </div>
+        </div>
+      )}
       <Toaster
         position="top-center"
-        reverseOrder={false}
-      />
+        reverseOrder={false} />
       <Routes>
-
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/' element={<Home />} />
-
+        <Route
+          path='/login'
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>} />
+        <Route
+          path='/register'
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>} />
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>} />
       </Routes>
     </Router>
   )
