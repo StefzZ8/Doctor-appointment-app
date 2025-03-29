2a1246a804819eeb8e7ee2a8fc999cc975644c15
Stefko1998 <Simjanovski998@gmail.com>
2025-03-29 19:20:34 +0100

    added: few more types

--- models/userModel.js
+++ models/userModel.js
@@ -1,20 +1,36 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  isDoctor: {
+    type: Boolean,
+    default: false
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  seenNotifications: {
+    type: Array,
+    default: [],
+  },
+  unseenNotifications: {
+    type: Array,
+    default: [],
+  },
 }, {
-    timestamps: true
+  timestamps: true
 })
 
 const userModel = mongoose.model("users", userSchema);
