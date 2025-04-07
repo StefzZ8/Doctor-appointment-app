132af2e5741d257028a97d0a37eec8b4bd474bde
Stefko1998 <Simjanovski998@gmail.com>
2025-04-07 11:20:27 +0200

    deleted: toast message: redirect to home

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -17,7 +17,6 @@ const Register = () => {
       dispatch(hideLoading());
       if (response.data.success) {
         toast.success(response.data.message);
-        toast('Redirecting to login page');
         navigate('/login');
       } else {
         toast.error(response.data.message);
